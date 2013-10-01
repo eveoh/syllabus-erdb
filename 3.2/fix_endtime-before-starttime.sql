4c880ac1a77fe8cbc61cf548b66b6b84019d3dde
Marco Krikke <marco@eveoh.nl>
2013-10-01 15:34:24 +0200

    Correct RDB 3.2 fix for activities where the end time can be before the start time when activities end at 0:00.

--- 3.2/fix_endtime-before-starttime.sql
+++ 3.2/fix_endtime-before-starttime.sql
@@ -17,37 +17,24 @@ GO
 
 ALTER VIEW [rdowner].[V_ACTIVITY_DATETIME]
 AS
-SELECT 
-t1.ActivityID,
-rdowner.AddTimeAndDayToDate(t2.StartTime,t2.DayNumber,t5.WeekStartDate) StartDateTime,
-rdowner.AddTimeAndDayToDate(t3.EndTime, 
-	CASE 
-		WHEN ((DATEPART(HH,t3.EndTime)) = 0 AND (DATEPART(MI, t3.EndTime)) = 0) 
-			THEN (t3.DayNumber + 1) 
-			ELSE t3.DayNumber 
-		END
-,t5.WeekStartDate) EndDateTime,
-t2.DayNumber Day,
-t4.Week Week,
-DENSE_RANK() OVER (PARTITION BY t1.ActivityId ORDER BY rdowner.AddTimeAndDayToDate(t2.StartTime,t2.DayNumber,t5.WeekStartDate)) Occurrence,
-tr.DateTime LastChanged
-FROM(
-SELECT ActivityID
-      ,Max(Period) MaxPeriod
-      ,Min(Period) MinPeriod
-      ,Max(LatestTransaction) LatestTransaction
-      ,PeriodIndex
-  FROM rdowner.ACTIVITY_SCHEDULEDPERIODS
-  GROUP BY ActivityId, PeriodIndex) t1
-  INNER JOIN rdowner.PERIODSTARTTIMES t2 
-  ON t1.MinPeriod = t2.PeriodIndex
- INNER JOIN rdowner.PERIODSTARTTIMES t3 
-  ON t1.MaxPeriod = t3.PeriodIndex
-INNER JOIN rdowner.ACTIVITY_SCHEDULEDWEEKS t4 
-	ON t1.ActivityID = t4.ActivityId 
-INNER JOIN rdowner.WEEK_LABELS t5
-	ON t4.Week = t5.WeekNumber
+SELECT	t1.ID ActivityId, 
+		[rdowner].AddTimeAndDayToDate(t1.StartTime,t1.DayIndex,t3.WeekStartDate) StartDateTime,
+		[rdowner].AddTimeAndDayToDate(t1.EndTime,
+			CASE 
+				WHEN ((DATEPART(HH,t1.EndTime)) = 0 AND (DATEPART(MI, t1.EndTime)) = 0) 
+					THEN (t1.DayIndex + 1) 
+					ELSE t1.DayIndex 
+				END
+		,t3.WeekStartDate) EndDateTime,
+		t1.DayIndex Day,
+		t2.Week,
+		tr.[DateTime] as [LastChanged] 
+FROM  rdowner.ACTIVITYPERIODTIMES as t1 
+INNER JOIN rdowner.ACTIVITY_SCHEDULEDWEEKS t2 
+	ON t1.ID = t2.ActivityId 
+INNER JOIN rdowner.WEEK_LABELS t3
+	ON t2.Week = t3.WeekNumber
 INNER JOIN rdowner.SD_Transaction tr 
 	ON tr.TransNum = t1.LatestTransaction
-
+	
 GO
